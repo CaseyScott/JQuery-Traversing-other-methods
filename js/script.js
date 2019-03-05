5f0b10d35bdac65da9cd386c8af82ee3691b91c5
casey scott <csnz2018@hotmail.com>
2019-03-05 17:28:58 +0000

    when reset is clicked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
- // put your code here
+    /*$("#panel").click(function(){
+        $(this).children(".container").hide();
+    });
+    
+    $(".superButton").click(function(){
+        $(".container").show();
+    });
+    
+    $(".container").click(function(){
+        $(this).hide();
+    })
+    
+    $(".superButton").click(function(){
+        $(".container").show();
+    });
+    
+    $(".container").click(function(){
+        $(this).children(".theButton").hide();
+    })
+    
+    $(".superButton").click(function(){
+        $(".theButton").show();
+    });*/
+    
+    $(".container").click(function(){
+        $(this).siblings().fadeTo('slow', 0.1);
+    });
+    
 });
