f7d099e889e485bab1ae951e4500c04eabaab522
casey scott <csnz2018@hotmail.com>
2019-05-31 00:25:13 +0000

    js/scripts modified

--- js/script.js
+++ js/script.js
@@ -23,21 +23,28 @@ $(document).ready(function(){
         $(".theButton").show();
     });*/
     
-    /*$(".container").click(function(){
-        $(this).siblings().fadeTo('slow', 0.1);
-    });
-    
-    $(".superButton").click(function(){
-        $(".container").fadeTo('slow', 1.0);
-    });*/
-    
-    $(".theButton").mouseenter(function(){
+   /* $(".theButton").mouseenter(function(){
         $(this).addClass("makeBlack");
     });
     
     $(".theButton").mouseout(function(){
         $(this).removeClass("makeBlack");
+    });*/
+    
+    $('.container').click(function(){
+        $(this).siblings().fadeTo('slow', 0.1);
+    });
+    
+    $('.superButton').click(function(){
+        $('.container').fadeTo('slow', 1.0);
     });
     
     
+  
+//will display rgb value of selected panel in the reset panel
+$(".theButton").click(function(){
+    var col = $(this).css('background-color');
+  $('.superButton').text( col);
+
+   });
 });
